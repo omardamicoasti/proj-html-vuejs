9f45c6a8062ac34441c2223e6f996a489db9bc6b
Omar D'Amico <omar.damico.asti@gmail.com>
2021-04-12 16:54:49 +0200

    Primo push
    header

---
+++ app.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+  el: "#app",
+
+  mounted() {},
+
+  data: {
+    headerCenterMenu: ["home", "pages", "courses", "features", "blog", "shop"],
+    languages: [
+      { flag: "en", name: "english" },
+      { flag: "de", name: "german" },
+      { flag: "fr", name: "french" },
+    ],
+  },
+
+  methods: {},
+});
+Vue.config.devtools = true;
+
+
+
